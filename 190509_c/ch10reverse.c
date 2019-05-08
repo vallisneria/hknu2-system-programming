d20b978837242c4d5a43c206e0a7f18829db1f86
Lee GyeongMuk <moogi3469@gmail.com>
2019-05-08 20:50:20 +0900

    5월 8일 실습 추가

---
+++ 190509_c/ch10reverse.c
@@ -0,0 +1,31 @@
+/*
+    2019년 5월 8일
+    시스템프로그래밍
+
+    텍스트 파일을 역순으로 출력하는 프로그램 ch10reverse.c를 작성하라.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if(argc!=3){
+        fprintf(stderr,"사용 방법: ch10reverse [입력파일] [출력파일]");
+        return 1;
+    }
+
+    FILE *fin, *fout;
+    char buffer=0;
+    
+    fin=fopen(argv[1],"r");
+    fout=fopen(argv[2],"w");
+
+    fseek(fin,0,SEEK_END);
+    do{
+        fseek(fin,-2*sizeof(buffer),SEEK_CUR);
+        fread(buffer,sizeof(buffer),1,fin);
+        fprintf(fout,"%c",buffer);
+    }while(ftell(fin)!=SEEK_SET);
+
+    fclose(fin);
+    fclose(fout);
+}
