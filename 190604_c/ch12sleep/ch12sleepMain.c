a11dac03df55e4de021d1eb9363c2d6b28b0938b
Vallisneria <moogi3469@gmail.com>
2019-06-04 01:10:04 +0900

    6월 4일 실습

--- 190604_c/ch12sleep/ch12sleepMain.c
+++ 190604_c/ch12sleep/ch12sleepMain.c
@@ -1,2 +1,12 @@
+#include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
 #include "ch12sleep.h"
+
+int main(int argc, char* argv[]) {
+    printf("sleep 시작\n");
+    ch12sleep(2);
+    printf("sleep 종료\n");
+    return 0;
+}
