e7e3367e4944b7ab20f115f66fa192b012cc9577
Vallisneria <moogi3469@gmail.com>
2019-06-12 21:02:16 +0900

    6월 13일 과제

--- Chapter13/2_NamedPipe/1_NamedPipe/student.c
+++ Chapter13/2_NamedPipe/1_NamedPipe/student.c
@@ -25,13 +25,10 @@ void stdin_clear() {
 // 질문을 입력받고 teacher로 보냄
 int main(int argc, char* argv[]) {
     // 초기 설정
-    int Q_fd_w, A_fd_r, teacher_pid, mypid = getpid();
-    char question[BUFSIZ], response[BUFSIZ];
+    int question, response, Q_fd_w, A_fd_r, teacher_pid, mypid = getpid();
     signal(SIGUSR1, sigusr1);
 
     // Named Pipe 설정
-    printf("Named Pipe 설정\n");
-
     mkfifo("QuestionPipe", 0660);
     mkfifo("AnswerPipe", 0660);
 
@@ -39,39 +36,34 @@ int main(int argc, char* argv[]) {
     A_fd_r = open("AnswerPipe", O_RDONLY);
 
     // 상대 프로세스에게 시그널을 보내기 위한 PID 교환 (생략가능)
-    printf("PID 교환\n");
     write(Q_fd_w, &mypid, sizeof(int));
     read(A_fd_r, &teacher_pid, sizeof(teacher_pid));
 
     // 파이프가 제대로 연결되었는지 확인하는 과정 (생략가능)
     // 서로 "연결 성공"이 출력되지 않으면 파이프가 제대로 연결되지 않은 것이다.
-    printf("시그널 전송\n");
     kill(teacher_pid, SIGUSR1);
+    sleep(1);
 
     printf("파이프를 통한 질문-답변 프로그램.\n");
-    printf("질문으로 0을 입력받으면 종료합니다.\n");
-
+    printf("질문으로 숫자를 입력하면 3을 더해서 출력합니다.\n");
+    printf("0을 입력받으면 종료합니다.\n");
     do {
         // 질문을 입력받음
         printf("Q: ");
-        scanf("%s", question);
-
-        if (!strcmp(question, "0")) {
-            break;
-        }
+        scanf("%d", &question);
 
         // 질문을 teacher로 보냄
-        write(Q_fd_w, question, strlen(question) + 1);
+        write(Q_fd_w, &question, sizeof(int));
 
         // 답변을 받음
-        read(A_fd_r, response, BUFSIZ);
+        read(A_fd_r, &response, BUFSIZ);
 
         // 그 답변을 출력함
-        printf("A: %s", response);
+        printf("A: %d\n\n", response);
 
         // 다음 질문을 입력받기 전 입력버퍼를 초기화
         stdin_clear();
-    } while (strcmp(question, "0"));
+    } while (question != 0);
 
     close(Q_fd_w);
     close(A_fd_r);
--- Chapter13/2_NamedPipe/1_NamedPipe/teacher.c
+++ Chapter13/2_NamedPipe/1_NamedPipe/teacher.c
@@ -19,13 +19,10 @@ void sigusr1() {
 // 답변을 받아서 question으로 보냄
 int main(int argc, char* argv[]) {
     // 초기 설정
-    int Q_fd_r, A_fd_w, Question_pid, mypid = getpid();
-    char question[BUFSIZ], response[BUFSIZ];
+    int question, response, Q_fd_r, A_fd_w, Question_pid, mypid = getpid();
     signal(SIGUSR1, sigusr1);
 
     // Named Pipe 설정
-    printf("Named Pipe 설정\n");
-
     mkfifo("QuestionPipe", 0660);
     mkfifo("AnswerPipe", 0660);
 
@@ -33,41 +30,24 @@ int main(int argc, char* argv[]) {
     A_fd_w = open("AnswerPipe", O_WRONLY);
 
     // 상대 프로세스에게 시그널을 보내기 위한 PID 교환 작업 (생략가능)
-    printf("PID 교환\n");
-
     write(A_fd_w, &mypid, sizeof(int));
     read(Q_fd_r, &Question_pid, sizeof(Question_pid));
 
     // 파이프가 제대로 연결되었는지 확인하는 과정 (생략가능)
     // "연결 성공"이 출력되지 않으면 파이프가 제대로 연결되지 않은 것이다.
-    printf("시그널 전송\n");
-
     kill(Question_pid, SIGUSR1);
+    sleep(1);
 
     do {
         // 질문을 받음
-        read(Q_fd_r, question, BUFSIZ);
-        if (!strcmp(question, "0")) {
-            break;
-        }
+        read(Q_fd_r, &question, BUFSIZ);
 
-        // 미리 입력해둔 질문들에서 적절한 답변을 찾음
-        if (!strcmp(question, "Hello")) {
-            strcpy(response, "World!");
-        } else if (!strcmp(question, "Your_name")) {
-            strcpy(response, argv[0]);
-        } else if (!strcmp(question, "the_answer_to_life_the_universe_and_everything")) {
-            strcpy(response, "42");
-        } else if (!strcmp(question, "0")) {
-            //종료
-        } else {
-            strcpy(response, "what?");
-        }
+        // 계산함
+        response = question + 3;
 
         // 답변을 보냄
-        sprintf(response, "%s\n\n", response);
-        write(A_fd_w, response, strlen(response) + 1);
-    } while (strcmp(question, "0"));
+        write(A_fd_w, &response, sizeof(int));
+    } while (question != 0);
 
     close(Q_fd_r);
     close(A_fd_w);
